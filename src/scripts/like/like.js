9f1e7f89cdafbfa72c1c36cdbcdcee8cde5808c3
sobbaka <a.bagaev1989@gmail.com>
2023-02-24 10:58:58 +0400

    add like-dislike scripts

---
+++ src/scripts/like/like.js
@@ -0,0 +1,28 @@
+const enableLike = () => {
+  const likeBtnList = Array.from(document.querySelectorAll('.element__like-btn'));
+  likeBtnList.forEach((likeBtn) => {
+    likeBtn.addEventListener("click", (e) => {
+      toggleLikeState(likeBtn);
+    })
+  });
+}
+
+const like = (button) => {
+  button.nextElementSibling.innerHTML = parseInt(button.nextElementSibling.innerHTML) + 1;
+  button.classList.add("element__like-btn_is-active");
+}
+
+const dislike = (button) => {
+  button.nextElementSibling.innerHTML = parseInt(button.nextElementSibling.innerHTML) - 1;
+  button.classList.remove("element__like-btn_is-active");
+}
+
+const toggleLikeState = (button) => {
+  if (button.classList.contains("element__like-btn_is-active")) {
+    dislike(button);
+  } else {
+    like(button);
+  }
+}
+
+window.addEventListener("DOMContentLoaded", enableLike);
